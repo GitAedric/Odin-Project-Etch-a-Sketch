119dbe6f48877a7f0b63747b0495e95467292bfb
GitAedric <241666953+GitAedric@users.noreply.github.com>
2025-12-18 19:36:47 -0600

    Initial Files Prepared
    Initial HTML, CSS and JS filed created and
    prepared. Very basic CSS and JS outlines done,
    container created in HTML.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Odin-Project-Etch-a-Sketch
+
+This is the repository for the Etch-a-Sketch Odin
+Project task.
---
+++ javascript.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+
+const gridSqr = document.createElement("div");
+gridSqr.classList.add("sqr");
+
+container.appendChild(gridSqr);
