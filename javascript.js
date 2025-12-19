fd5de1b4090293212e5f8a9d7b565305c15b0575
GitAedric <241666953+GitAedric@users.noreply.github.com>
2025-12-18 20:14:51 -0600

    Began Initial Coding Process
    Grid of 16x16 divs successfully created using JS,
    in the process of testing event listener
    functionality.
    
    Known issue: container div is being included in
    event listener call.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
 const container = document.querySelector("#container");
 
-const gridSqr = document.createElement("div");
-gridSqr.classList.add("sqr");
 
-container.appendChild(gridSqr);
+
+for (let i = 0; i < 256; i++) {
+    const gridSqr = document.createElement("div");
+    gridSqr.classList.add("sqr");
+    container.appendChild(gridSqr);
+}
+
+const square = container.querySelectorAll(".sqr");
+
+square.forEach((div) => {
+        addEventListener("mouseover", function(e) {
+            e.target.style.background = "#2e2e2eff";
+        })
+    }
+);
